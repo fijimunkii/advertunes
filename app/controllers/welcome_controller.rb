6e054269654ef817bbcd9415adeb0bf5d6bbf959
Harrison Powers <fijimunkii@gmail.com>
2013-12-13 09:55:43 -0500

    flash message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,6 @@ class WelcomeController < ApplicationController
 
   def register
     Registration.create(email: params[:email])
-    redirect_to :root
+    redirect_to :root, notice: "Thanks for your interest! We'll email you when the service goes live."
   end
 end
