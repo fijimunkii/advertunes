fa108df2a5dcf98a108b15cdf0f657dc263a5701
Harrison Powers <fijimunkii@gmail.com>
2013-12-13 12:35:58 -0500

    above the fold

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
 
   App.router = new App.Router();
 
-  $('.call-to-action button').on('click', function() {
-    var $email = $('#email');
-    $email.focus();
-    focusZoom.trigger($email[0]);
-  });
+  var $email = $('#email');
+  $email.focus();
+  focusZoom.trigger($email[0]);
 
 });
