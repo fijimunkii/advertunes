b323fecf428521e3c938589dac7c3dea7a598163
Harrison Powers <fijimunkii@gmail.com>
2013-12-24 03:46:52 -0500

    uploads files ;

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,4 @@ class WelcomeController < ApplicationController
   def index
     @index = true
   end
-
-  def register
-    Registration.create(email: params[:email])
-    redirect_to :root, notice: "Thanks for your interest! We'll email you when the service goes live."
-  end
-
 end
